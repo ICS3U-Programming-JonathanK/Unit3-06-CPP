084ae5dd0470771571d84971458f375d74cb0bf9
Ubuntu <ubuntu@ip-172-31-18-62.ec2.internal>
2021-05-14 20:53:35 +0000

    added in numGuessTryCatch.cpp

--- numGuessTryCatch.cpp
+++ numGuessTryCatch.cpp
@@ -4,38 +4,35 @@
 // Created on: May 14 2021
 // This program asks the user to guess a number between 0 to 9
 // and tells the user if the guess is right or wrong
-// and tells the user if the guess is right or wrong
 
 #include <iostream>
 #include <random>
 
 int main() {
-  int userGuessAsInt, correctGuess;
-  std::string userGuessAsString;
+    int userGuessAsInt, correctGuess;
+    std::string userGuessAsString;
 
-
-    // generate a number between 0 to 9
-  std::random_device rseed;
-  std::mt19937 rgen(rseed());
-  std::uniform_int_distribution<int> idist(0, 9);
-  correctGuess = idist(rgen);
+    std::random_device rseed;
+    std::mt19937 rgen(rseed());
+    std::uniform_int_distribution <int> idist(0, 9);
+    correctGuess = idist(rgen);
 
     // get the user's guess
-  std::cout << "Enter a number between 0 to 9: ";
-  std::cin >> userGuessAsString;
+    std::cout << "Enter a number between 0 to 9: ";
+    // get the guess from the user as a string
+    std::cin >> userGuessAsString;
 
-// get the guess from the user as a string
+    // check if the guess is correct
+    if (userGuessAsInt == correctGuess) {
+        std::cout << "You are correct!";
+    } else {
+        std::cout << "You are wrong! The answer is = ";
+        std::cout << correctGuess << std::endl;
+    }
 
 try {
     // convert the user's guess to an int
     userGuessAsInt = std::stoi(userGuessAsString);
-
-    // check to see if the user guess is correct or wrong
-    if (userGuessAsInt == correctGuess) {
-    std::cout << "You are correct!";
-    } else {
-    std::cout << "You are wrong! The answer is = " << correctGuess << std::endl;
-    std::cout << correctGuess << std::endl;
 } catch (std::invalid_argument) {
     // The user did not enter an integer.
     std::cout << userGuessAsString << " is not an integer.\n";
